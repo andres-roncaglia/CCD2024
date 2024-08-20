e59a606ff1a59c9eb2a6ff30e27059fc17afc39b
andrew <andresroncaglia11@yahoo.com.ar>
2024-08-20 19:14:22 -0300

    La pagina ahora se maneja solo con dashboard
    Agregado un mapa interactivo
    
    Agregado link a github

--- app.R
+++ app.R
@@ -7,10 +7,9 @@ library(tidyverse)
 library(ggplot2)
 library(plotly)
 library(readxl)
-library(tidyr)
-library(dplyr)
+library(RColorBrewer)
 
-# Carga de datos -------------
+# Carga de datos https://data.undp.org/access-all-data-------------
 Ciencia <- read_xlsx("Datos/Ciencia y cambios tecnologicos.xlsx")
 Vida <- read_xlsx("Datos/Condiciones de vida.xlsx")
 Economia <- read_xlsx("Datos/Desarrollo economico.xlsx")
@@ -19,6 +18,29 @@ Poblacion <- read_xlsx("Datos/Poblacion y cambios demograficos.xlsx")
 Pobreza <- read_xlsx("Datos/Pobreza.xlsx")
 Salud <- read_xlsx("Datos/Salud.xlsx")
 Trabajo <- read_xlsx("Datos/Trabajo.xlsx")
+
+# Carga de datos https://ourworldindata.org/ ---------------
+
+transporte_publico <- read.csv("Datos/sub - acceso transporte publico.csv") |> 
+  rename(Valor = Proportion.of.population.that.has.convenient.access.to.public.transport,
+         Año = Year,
+         Codigo = Code,
+         Pais = Entity) |> 
+  mutate(Indicador = "Porcentaje de la poblacion con acceso conveniente al transporte público")
+
+emisiones <- read.csv("Datos/sub - emisiones de co2.csv")|> 
+  rename(Valor = Per.capita.carbon.dioxide.emissions.from.transport,
+         Año = Year,
+         Codigo = Code,
+         Pais = Entity) |> 
+  mutate(Indicador = "Emisiones de dioxido de carbono por el transporte per capita")
+
+gasto_investigacion <- read.csv("Datos/sub - gasto en investigacion.csv") |> 
+  rename(Valor = Research.and.development.expenditure....of.GDP.,
+         Año = Year,
+         Codigo = Code,
+         Pais = Entity) |> 
+  mutate(Indicador = "Porcentaje del GDP invertido en investigación y desarrollo")
 
 # Preparación de bases de datos --------------
 prep <- function(x) {
@@ -33,79 +55,321 @@ prep <- function(x) {
       Pais = case_when(
         Pais == "Bolivia (Plurinational State of)" ~ "Bolivia",
         Pais == "Venezuela (Bolivarian Republic of)" ~ "Venezuela", 
-        T ~ Pais))
+        T ~ Pais),
+      Año = as.numeric(Año))
 }
 
-Ciencia <- prep(Ciencia)
-Vida <- prep(Vida)
-Economia <- prep(Economia)
-Educacion <- prep(Educacion)
-Poblacion <- prep(Poblacion)
-Pobreza <- prep(Pobreza)
-Salud <- prep(Salud)
-Trabajo <- prep(Trabajo)
+Ciencia <- prep(Ciencia) 
+Ciencia <- Ciencia |> 
+  bind_rows(
+    filter(transporte_publico, Codigo %in% unique(Ciencia$Codigo)),
+    filter(emisiones, Codigo %in% unique(Ciencia$Codigo)),
+    filter(gasto_investigacion, Codigo %in% unique(Ciencia$Codigo))) |> 
+  arrange(Pais) |> 
+  filter(!(Indicador %in% c("Percentage of primary schools with access to the internet", "Percentage of secondary schools with access to the internet")))
 
-Ciencia |> 
-  filter(Indicador == "Access to internet, percent of population") |> 
-  ggplot(aes(x = Año, y = Valor, color = Pais, group = Pais)) +
-  geom_point() +
-  geom_line()
+Vida <- prep(Vida) |> arrange(Pais)
+Economia <- prep(Economia) |> arrange(Pais)
+Educacion <- prep(Educacion) |> arrange(Pais)
+Poblacion <- prep(Poblacion) |> arrange(Pais)
+Pobreza <- prep(Pobreza) |> arrange(Pais)
+Salud <- prep(Salud) |> arrange(Pais)
+Trabajo <- prep(Trabajo) |> arrange(Pais)
 
 
-# Página principal
-general_view <- function() {
-  fluidPage(
-    h2("Welcome to the App"),
-    p("Click the button to go to the detailed view."),
-    actionButton("go_to_details", "Go to Detailed Page")
-  )
-}
 
+
+
+# Mapa de sudamerica ----------------
+library(leaflet)
+library(spData)
+library(sf)
+
+data(world)
+
+datos_mapa <- filter(world, continent == "South America") |> 
+  arrange(name_long) |> 
+  filter(iso_a2 != "FK")
+
+mapa <- datos_mapa |> 
+  leaflet() |> 
+  addTiles() |> 
+  setMaxBounds(lng1 = -22, lng2 = -98, lat1 = -61, lat2 = 20) |> 
+  setView(lng = -58, lat = -23, zoom = 2.6)
 
 # Interfaz ------------
 
 
-ui <- fluidPage(
+ui <- dashboardPage(
   
-  uiOutput("main_ui")
-
   
+  dashboardHeader(titleWidth = "0%"),
+  
+  ## Barra de opciones ----------------
+  dashboardSidebar(collapsed = T,
+                   sidebarMenu(
+                     id = "sidebar",
+                     menuItem("Página principal", tabName = "pag_principal", icon = icon("home")),
+                     menuItem("Ciencia", tabName = "ciencia", icon = icon("microscope")),
+                     menuItem("Vida", tabName = "vida", icon = icon("hand-holding-heart")),
+                     menuItem("Economia", tabName = "economia", icon = icon("money-bill-wave")),
+                     menuItem("Educacion", tabName = "educacion", icon = icon("user-graduate")),
+                     menuItem("Poblacion", tabName = "poblacion", icon = icon("users")),
+                     menuItem("Pobreza", tabName = "pobreza", icon = icon("person-shelter")),
+                     menuItem("Salud", tabName = "salud", icon = icon("heart-pulse")),
+                     menuItem("Trabajo", tabName = "trabajo", icon = icon("briefcase"))
+                     )
+                   ),
+  
+  
+  dashboardBody(
+    
+    tabItems(
+      
+      # Página principal ------------------
+      tabItem(
+        tabName = "pag_principal",
+        
+        h2("Contar con datos 2024", style = "text-align: center;"),
+        p("Click the button to go to the detailed view."),
+        
+        ## Primera hilera de botones ---------------------
+        fluidRow(
+          column(3,
+                 actionBttn("ciencia", "Ciencia", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("vida", "Vida", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("economia", "Economia", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("educacion", "Educacion", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          )
+        ),
+        
+        ## Segunda hilera de botones ---------------------
+        fluidRow(
+          column(3,
+                 actionBttn("poblacion", "Poblacion", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("pobreza", "Pobreza", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("salud", "Salud", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("trabajo", "Trabajo", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          )
+        ),
+        
+        ## Tercera hilera de botones ---------------------
+        fluidRow(
+          column(3,
+                 actionBttn("go_to_details", "Go to Detailed Page", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("go_to_details", "Go to Detailed Page", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("go_to_details", "Go to Detailed Page", block = T, style = "fill", size = "lg", no_outline = F, color = "royal"),
+          ),
+          column(3,
+                 actionBttn("github", "Github", block = T, style = "fill", size = "lg", no_outline = F, color = "royal", icon = icon("github"))
+          )
+        )
+        
+        
+      ),
+      
+      
+      # Pagina Ciencia -------------
+      
+      tabItem(
+        tabName = "ciencia",
+        
+        h2("La ciencia en Latinoamerica"),
+        
+        fluidRow(column(7, 
+                        pickerInput(inputId = "indicador",
+                                    label = "Variable",
+                                    unique(Ciencia$Indicador))),
+                 column(5,
+                        sliderTextInput(inputId = "mapa_anio",
+                                    label = "Año",
+                                    grid = T,
+                                    choices = sort(unique(Ciencia$Año)),
+                                    selected = max(Ciencia$Año)
+                                        ))),
+        
+        fluidRow(
+          column(7,
+                 plotlyOutput("plot_evo_ciencia")
+                 ),
+          
+          column(5,
+                 leafletOutput("mapa_ciencia", width = "100%", height = "400px")
+                 )
+        )
+        
+        
+        
+        )
+    )
+  )
 )
+
 
 
 # Servidor ----------------
 server <- function(input, output, session) {
   
-  ## Menu principal -------------------
-  output$main_ui <- renderUI({
-    general_view()
+  ## Botones a cada una de las secciones ----------------------
+  observeEvent(input$ciencia, {
+    updateTabItems(session, "sidebar", selected = "ciencia")
   })
   
-  observeEvent(input$go_to_details, {
+  observeEvent(input$vida, {
+    updateTabItems(session, "sidebar", selected = "vida")
+  })
+  
+  observeEvent(input$economia, {
+    updateTabItems(session, "sidebar", selected = "economia")
+  })
+  
+  observeEvent(input$educacion, {
+    updateTabItems(session, "sidebar", selected = "educacion")
+  })
+  
+  observeEvent(input$poblacion, {
+    updateTabItems(session, "sidebar", selected = "poblacion")
+  })
+  
+  observeEvent(input$pobreza, {
+    updateTabItems(session, "sidebar", selected = "pobreza")
+  })
+  
+  observeEvent(input$salud, {
+    updateTabItems(session, "sidebar", selected = "salud")
+  })
+  
+  observeEvent(input$trabajo, {
+    updateTabItems(session, "sidebar", selected = "trabajo")
+  })
+  
+  observeEvent(input$github, {
+    browseURL("https://github.com/andres-roncaglia/CCD2024")
+  })
+  
+  ## Ciencia -------------------------------
+  
+  ### Grafico evolutivo ----------------
+  
+  output$plot_evo_ciencia <- renderPlotly({
+  
+    graf <- Ciencia |> 
+      filter(Indicador == input$indicador) |> 
+      ggplot(aes(x = Año, y = Valor, color = Pais, group = Pais)) +
+      geom_point() +
+      geom_line() +
+      ylab(label = input$indicador)
     
-    ## Pagina 1 -----------------
-    output$main_ui <- renderUI({
-      sidebarLayout(
-        sidebarPanel(
-          h3("Detailed Sidebar"),
-          # Add more sidebar content here
-          p("This is a detailed page with a sidebar."),
-          actionButton("go_back", "Back to Main Page")
-        ),
-        mainPanel(
-          h2("Detailed Content"),
-          p("Here is where you put more detailed content."),
-          # Add your detailed UI elements here
+    ggplotly(graf)
+  })
+
+  
+  ### Grafico correlaciones ----------------
+  
+  output$plot_corr <- renderPlotly({
+    
+    if (input$var_corr_2 %in% Ciencia$Indicador) {
+      datos_corr = filter(Ciencia, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Vida$Indicador) {
+      datos_corr = filter(Vida, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Economia$Indicador) {
+      datos_corr = filter(Economia, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Educacion$Indicador) {
+      datos_corr = filter(Educacion, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Poblacion$Indicador) {
+      datos_corr = filter(Poblacion, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Pobreza$Indicador) {
+      datos_corr = filter(Pobreza, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Salud$Indicador) {
+      datos_corr = filter(Salud, Indicador == input$var_corr_2)
+    } else if (input$var_corr_2 %in% Trabajo$Indicador) {
+      datos_corr = filter(Trabajo, Indicador == input$var_corr_2)
+    }
+    
+    datos_corr <- Ciencia |> 
+      filter(Año == input$anio_corr, Indicador == input$indicador) |> 
+      inner_join(datos_corr, unmatched = "drop", by = c("Pais","Codigo", "Año")) |> 
+      drop_na()
+
+      graf <- 
+        datos_corr |> ggplot(aes(x = Valor.x, y = Valor.y, color = Pais), color = "dodgerblue") +
+        geom_point() +
+        xlab(label = input$indicador) +
+        ylab(label = input$var_corr_2) +
+        annotate(geom = "text", 
+                 label = ifelse(nrow(datos_corr) == 0, "No hay datos que coincidan con los filtros aplicados", paste("Correlación:", cor(datos_corr$Valor.x, datos_corr$Valor.y))),
+                 x = ifelse(nrow(datos_corr) == 0, 1, mean(datos_corr$Valor.x)),
+                 y = ifelse(nrow(datos_corr) == 0, 1, max(datos_corr$Valor.y)*0.7)
         )
-      )
-    })
+        
+    
+    ggplotly(graf)
   })
   
+  ### Mapa ciencia -----------------------
   
-  observeEvent(input$go_back, {
-    output$main_ui <- renderUI({
-      general_view()
-    })
+  # Actualizador de la barra para seleccionar año
+  observeEvent(input$indicador, {
+    
+    updateSliderTextInput(
+      session = session,
+      inputId = "mapa_anio",
+      choices = sort(unique(filter(Ciencia, Indicador == input$indicador)$Año)),
+      selected = max(filter(Ciencia, Indicador == input$indicador)$Año)
+    )
+  })
+  
+  # Maapa
+  output$mapa_ciencia <- renderLeaflet({
+    
+    datos <- filter(Ciencia, Indicador == input$indicador, Año == input$mapa_anio)
+    
+    
+    pal <- colorNumeric(
+      palette = "RdYlGn",
+      domain = filter(Ciencia, Indicador == input$indicador)
+    )
+    
+    Labels <- sprintf(
+      "<strong>%s</strong><br/>%g",
+      datos$Pais, 
+      datos$Valor
+    ) %>% lapply(htmltools::HTML)
+    
+    mapa |> 
+      addPolygons(fillColor = ~pal(datos$Valor),
+                  weight = 2,
+                  opacity = 1,
+                  color = "white",
+                  dashArray = "3",
+                  fillOpacity = 1,
+                  highlightOptions = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  label = Labels,
+                  labelOptions = labelOptions(
+                    style = list("font-weight" = "normal", padding = "3px 8px"),
+                    textsize = "15px",
+                    direction = "auto"))
   })
   
 }
